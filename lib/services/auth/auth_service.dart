8edbeedf47177cc82b45a054246d03f7e2bbbf75
Seshan <zeshan.nandan@gmail.com>
2020-02-17 15:50:35 +0530

    auth - halfway done

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: [
+    'email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+  ]);
+
+  Future<bool> anonymousSignIn() async {
+    return (await _auth.signInAnonymously()) != null;
+  }
+
+  Future<bool> signIn() async {
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    final AuthResult authResult = await _auth.signInWithCredential(credential);
+    if (authResult.additionalUserInfo.isNewUser) {}
+    return authResult == null;
+  }
+
+  Future<void> logout() async {
+    if ((await _auth.currentUser()).isAnonymous) {
+      
+    }
+
+    _auth.signOut();
+    _googleSignIn.signOut();
+  }
+
+  Stream<FirebaseUser> get user {
+    return _auth.onAuthStateChanged;
+  }
+}
